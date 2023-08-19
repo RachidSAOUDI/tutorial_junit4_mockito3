ecb3294e8c6a4dd25a6060ca89a2740a13151f29
RachidSAOUDI <102986947+RachidSAOUDI@users.noreply.github.com>
2023-08-19 16:26:35 +0100

    Create Calculator.java

---
+++ src/main/java/com/tutorials/junit/Calculator.java
@@ -0,0 +1,10 @@
+package com.tutorials.junit;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    private int substract(int a, int b) {
+        return a-b;
+    }
+}
